a02f8e2ac6dd69cfd24806570aa5ca70e196e17d
SeaweedbrainCY <nathanstchepinsky@gmail.com>
2025-05-02 12:27:26 -0400

    Add missing changelog

--- frontend/src/app/changelog/changelog.component.ts
+++ frontend/src/app/changelog/changelog.component.ts
@@ -15,6 +15,51 @@ export class ChangelogComponent {
 
   changelogs = [
     {
+      date: "02/05/2025",
+      version: "1.6",
+      added: [
+        "Rescue Zero-TOTP now help you knowing when Zero-TOTP is back online !",
+        
+        
+      ],
+      fixed:[
+        "Some UI improvements.",
+        "We fixed several CVE vulnerabilities from the dependencies.",
+      ]
+    },
+    {
+      date: "05/12/2024",
+      version: "1.5",
+      added: [
+        "Following Zero-TOTP new features, Rescue Zero-TOTP is now also available as a PWA application ! Install it on your phone or desktop and use it offline ! ",
+        
+      ],
+      fixed:[
+        
+      ]
+    },
+    {
+      date: "20/11/2024",
+      version: "1.4",
+      added: [
+        "Rescue Zero-TOTP is now warning you when Zero-TOTP is encountering issue."
+        
+      ],
+      fixed:[
+        
+      ]
+    },
+    {
+      date: "14/07/2024",
+      version: "1.3",
+      added: [
+        
+      ],
+      fixed:[
+        "We fixed several CVE vulnerabilities from the dependencies.",
+      ]
+    },
+    {
       date: "03/04/2024",
       version: "1.1",
       added: [
